b3e3586607e54f66616cbd476f2ecbb6bec30219
igorcaf3 <51419192+igorcaf3@users.noreply.github.com>
2021-01-21 17:17:59 -0300

    Update Connect.php
    Atualização nas opções de inicialização

--- MSSQL/source/Database/Connect.php
+++ MSSQL/source/Database/Connect.php
@@ -17,7 +17,7 @@ class Connect
      * OPÇÕES DE INICIALIZAÇÃO
      */
     private const OPTIONS = [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+       // PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_CASE => PDO::CASE_NATURAL,
